7504c4ee91a1d0159f4bb7b92f7f930f7b283260
visasnouski <visasnouski@gmail.com>
2018-02-08 09:48:00 +0300

    Regex Split

--- Task/BaseClasses/ParserDI.cs
+++ Task/BaseClasses/ParserDI.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,11 @@ namespace Task
         public Dictionary<string, int> GetCityDictionary(IEnumerable<string> listEnum)
         {
             Dictionary<string, int> dc = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            Regex regex = new Regex(",");
             foreach (var x in listEnum)
             {
-                string key = GetName(x);
-                int value = GetPopulation(x);
+                string key = regex.Split(x)[0];
+                int value =Convert.ToInt32(regex.Split(x)[1]);
                 if (dc.ContainsKey(key))
                 {
                     dc[key] += value;
